08511462677898893885c01de9fe45562e4aeea9
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2021-08-16 16:40:10 -0600

    commit 2

--- Computadora/Computadora/Negocio/NClsComputadora.cs
+++ Computadora/Computadora/Negocio/NClsComputadora.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Computadora.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Computadora.Negocio
 {
     class NClsComputadora
+
     {
+        public String Apagar(ClsComputadora computadora)
+        {
+            
+            return "La computadora con tipo de " + computadora.Camara + "con un microprocesador  " + computadora.Nombremicroprocesador + " se esta apagando";
+
+
+        }
+        public String Almacenar(ClsComputadora computadora)
+        {
+            
+            return "La computadora con un disco duro de" + computadora.Tamañodiscoduro + "GB" + " con un tipo de monitor " + computadora.Monitor + " almacena informacion ";
+        }
+        public String Volumen(ClsComputadora computadora)
+        {
+            
+            return "La computadora con un microfono " + computadora.Microfono + "y bocinas" + computadora.Bocinas + " tiene un excelente volumen";
+        }
+        
     }
 }
+
