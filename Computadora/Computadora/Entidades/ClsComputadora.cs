d8a17eded04f208ec5d9865aa4e6f65250c2ddf8
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2021-08-16 15:06:31 -0600

    Commit Computadora

---
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Computadora.Entidades
+{
+    class ClsComputadora
+    {
+    }
+}
---
+++ Computadora/Computadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Computadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
