407a39b227d26b6b18362164237f4d73df44fbea
Frank-Rj <87914653+Frank-Rj@users.noreply.github.com>
2021-08-16 15:50:55 -0600

    Encapsulamiento

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -6,5 +6,28 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
+        private int tamañomemoria;
+        private int tamañoram;
+        private String nombremicroprocesador;
+        private int tamañodiscoduro;
+        private String teclado;
+        private String nombreplacamadre;
+        private String monitor;
+        private String bocinas;
+        private String camara;
+        private String microfono;
+        private String mouse;
+
+        public int Tamañomemoria { get => tamañomemoria; set => tamañomemoria = value; }
+        public int Tamañoram { get => tamañoram; set => tamañoram = value; }
+        public string Nombremicroprocesador { get => nombremicroprocesador; set => nombremicroprocesador = value; }
+        public int Tamañodiscoduro { get => tamañodiscoduro; set => tamañodiscoduro = value; }
+        public string Teclado { get => teclado; set => teclado = value; }
+        public string Nombreplacamadre { get => nombreplacamadre; set => nombreplacamadre = value; }
+        public string Monitor { get => monitor; set => monitor = value; }
+        public string Bocinas { get => bocinas; set => bocinas = value; }
+        public string Camara { get => camara; set => camara = value; }
+        public string Microfono { get => microfono; set => microfono = value; }
+        public string Mouse { get => mouse; set => mouse = value; }
     }
 }
